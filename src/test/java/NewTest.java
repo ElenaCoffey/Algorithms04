a77e83bf0bab0f4b45f7a527466466e2a9cdb560
Elena Coffey <elenacoffey@gmail.com>
2022-05-23 22:06:22 -0700

    HW9 #1

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,23 +1,5 @@
 public class NewTest {
 
-    //new test :git add /git commit/ git push
-//    Создать алгоритм OddEven, который принимает на вход целое число,
-//    возвращает “Odd”,  если число нечетное, и “Even”, если число четное.
-//    Во всех остальных случаях результат будет “Undefined”.
-//
-//    Test Data:
-//            -345 →  “Odd”
-//            0 →  “Even”
-//            222222 →  “Even”
-//            2147483647 + 1 →  “Undefined”
-
-    public static void OddEven( int n) {
-        if ( n % 2 != 0 ) {
-            System.out.println("Odd");
-        } else {
-            System.out.println("Even");
-        }
-    }
 
 
 
